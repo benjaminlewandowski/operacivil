383cd5aa8e831305db1066bd6c47e426fa0281de
benjamin lewandowski <benjamin.lewandowski@hmmh.de>
2025-11-16 20:10:25 +0100

    Improves navigation and index page layout
    Enhances the layout of the navigation component by adding conditional bottom margin and removing unnecessary height constraints.
    
    Refines the index page layout by adjusting padding and margins for improved visual appeal.

--- app/components/navigation.vue
+++ app/components/navigation.vue
@@ -12,14 +12,14 @@ const _sortedNavLinks = computed(() => {
 </script>
 
 <template>
-	<div class="h-full">
+	<div class="h-full mb-0">
   <nav class="px-4 pt-4 md:sticky md:top-0 md:max-h-screen md:overflow-auto">
 		<div class="bg-white/50 backdrop-blur-xs w-full mb-4 p-4">
 			<NuxtLink to="/" class="">
 				<img src="/images/logo.png" alt="Opera Civil Logo" class="w-full max-w-[416px] " />
 			</NuxtLink>
 		</div>
-    <ul class="flex gap-2 flex-wrap md:flex-col mb-4">
+    <ul class="flex gap-2 flex-wrap md:flex-col md:mb-4" :class="route.path !== '/' ? 'mb-4' : ''">
       <li v-for="link in _sortedNavLinks" :key="link.to">
         <NuxtLink :to="link.to" class="py-2 px-3 bg-white border-slate-300 border hover:bg-slate-200 block transform-border duration-500"
 				:class="route.path === link.to ? 'border-l-16' : null">{{ link.name }}</NuxtLink>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-	<div class="h-auto pt-4 pr-4 w-full  md:col-span-2">
-	<div class="prose p-4 mr-4 bg-white/50 backdrop-blur-xs w-full">
+	<div class="h-auto md:pt-4 pr-4 w-full  md:col-span-2">
+	<div class="prose p-4 mr-4 bg-white/50 backdrop-blur-xs w-full ml-4 md:ml-0">
 			<blockquote class="border-slate-500">{{ quotes[Math.floor(Math.random() * quotes.length)].quote }}<br><cite>– {{ quotes[Math.floor(Math.random() * quotes.length)].author }}</cite></blockquote>
 		</div>
 	</div>
