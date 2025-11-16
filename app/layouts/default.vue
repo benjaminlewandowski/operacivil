06226f68e84f37714f71be6899c06700324798bf
benjamin lewandowski <benjamin.lewandowski@hmmh.de>
2025-11-16 18:44:52 +0100

    Adds a quote to the index page
    Introduces a randomly selected quote to the index page to provide inspiration.
    
    The layout is also adjusted to use `1fr` for the row height, instead of `_1fr` which is invalid.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="grid grid-rows-[auto,_1fr] md:grid-cols-3 md:grid-rows-1 gap-4 max-w-7xl">
+	<div class="grid grid-rows-[auto,1fr] md:grid-cols-3 md:grid-rows-1 gap-4 max-w-7xl">
 		<Navigation />
     <slot />
   </div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,15 @@
+<script setup>
+	import { quotes } from "../assets/data/quotes";
+	const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+</script>
+
 <template>
-	<div />
+	<div
+		class="prose p-4 md:col-span-2"
+	>
+		<blockquote v-if="randomQuote">
+			{{ randomQuote.quote }}<br>
+			<cite>– {{ randomQuote.author }}</cite>
+		</blockquote>
+	</div>
 </template>
