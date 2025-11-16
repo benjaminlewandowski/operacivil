a136138162ff758882e0eaacc946119c0aa86893
benjamin lewandowski <benjamin.lewandowski@hmmh.de>
2025-11-16 19:55:47 +0100

    Decreases background image opacity
    Reduces the opacity of the background image and subtle background to improve readability of content.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -24,7 +24,7 @@ useRouter().afterEach(() => {
 	</div>
 
 	<picture class="fixed inset-0">
-		<img :src="`/images/bg${randomImage}.jpg`" :class="[loaded ? 'opacity-30' : 'opacity-0']"
+		<img :src="`/images/bg${randomImage}.jpg`" :class="[loaded ? 'opacity-10' : 'opacity-0']"
 			class="transition-opacity duration-2000 w-full h-screen object-cover z-0" />
 	</picture>
 </template>
@@ -33,8 +33,8 @@ useRouter().afterEach(() => {
 picture {
 
 	background:
-		radial-gradient(hsl(0 0% 0% / 25%) 0.5px, transparent 0.5px) 0 0 /3px 3px,
-		radial-gradient(hsl(0 0% 0% / 25%) 0.5px, transparent 0.5px) 1px 1px /3px 3px,
-		radial-gradient(hsl(0 0% 0% / 25%) 0.5px, transparent 0.5px) 2px 2px /3px 3px
+		radial-gradient(hsl(0 0% 0% / 20%) 0.5px, transparent 0.5px) 0 0 /3px 3px,
+		radial-gradient(hsl(0 0% 0% / 20%) 0.5px, transparent 0.5px) 1px 1px /3px 3px,
+		radial-gradient(hsl(0 0% 0% / 20%) 0.5px, transparent 0.5px) 2px 2px /3px 3px
 }
 </style>
